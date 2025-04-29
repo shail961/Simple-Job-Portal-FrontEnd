d955222d1e448c633e67a5515a3deb115f838520
Shaik Sahil Ahmed <sahilshaik000123@gmail.com>
2025-04-30 00:26:50 +0530

    Import change in JobDetailPage

--- src/pages/JobDetailPage.jsx
+++ src/pages/JobDetailPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "../api/axios";
 
 function JobDetailPage() {
   const { jobId } = useParams();
